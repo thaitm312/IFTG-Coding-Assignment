da3a14912e574681f02eb9222c6161a3f32d8424
Thai Tran <thaitm312@gmail.com>
2024-03-24 22:31:03 +0700

    [Booking] - Add Booking Logic

--- src/SettlementBookingSystem.Application/Bookings/Commands/CreateBookingCommandHandler.cs
+++ src/SettlementBookingSystem.Application/Bookings/Commands/CreateBookingCommandHandler.cs
@@ -1,6 +1,11 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Options;
+using SettlementBookingSystem.Application.Bookings.Context;
 using SettlementBookingSystem.Application.Bookings.Dtos;
+using SettlementBookingSystem.Application.Exceptions;
+using SettlementBookingSystem.Application.Options;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,14 +13,41 @@ namespace SettlementBookingSystem.Application.Bookings.Commands
 {
     public class CreateBookingCommandHandler : IRequestHandler<CreateBookingCommand, BookingDto>
     {
-        public CreateBookingCommandHandler()
+        private readonly IOptions<BookingOptions> _options;
+
+        private readonly IBookingContext _context;
+
+        public CreateBookingCommandHandler(IOptions<BookingOptions> options, IBookingContext context)
         {
+            _options = options;
+            _context = context;
         }
 
         public Task<BookingDto> Handle(CreateBookingCommand request, CancellationToken cancellationToken)
         {
-            // TODO Implement CreateBookingCommandHandler.Handle() and confirm tests are passing. See InfoTrack Global Team - Tech Test.pdf for business rules.
-            throw new NotImplementedException();
+            var startTime = TimeSpan.Parse(request.BookingTime);
+            var endTime = startTime.Add(TimeSpan.FromHours(_options.Value.DurationInHours));
+
+            var bookingCounts = _context.Bookings.Count(x => x.Start < endTime && x.End > startTime);
+
+            if (bookingCounts >= _options.Value.SimultaneousSettlements)
+            {
+                throw new ConflictException($"Booking settlements limit reached for slot {request.BookingTime}");
+            }
+
+            var entity = new BookingEntity
+            {
+                Name = request.Name,
+                Start = startTime,
+                End = endTime
+            };
+
+            _context.Bookings.Add(entity);
+
+            return Task.FromResult(new BookingDto
+            {
+                BookingId = entity.BookingId
+            });
         }
     }
 }
