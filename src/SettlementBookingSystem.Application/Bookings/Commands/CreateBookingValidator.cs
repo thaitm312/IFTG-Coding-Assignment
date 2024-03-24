973f1dfbb7f311acad61a46a6de22b80dfc2c4d9
Thai Tran <thaitm312@gmail.com>
2024-03-24 22:30:33 +0700

    [Booking] - Add Booking Validation

--- src/SettlementBookingSystem.Application/Bookings/Commands/CreateBookingValidator.cs
+++ src/SettlementBookingSystem.Application/Bookings/Commands/CreateBookingValidator.cs
@@ -1,13 +1,22 @@
 ﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using SettlementBookingSystem.Application.Options;
+using System;
 
 namespace SettlementBookingSystem.Application.Bookings.Commands
 {
     public class CreateBookingValidator : AbstractValidator<CreateBookingCommand>
     {
-        public CreateBookingValidator()
+        public CreateBookingValidator(IOptions<BookingOptions> options)
         {
+            CascadeMode = CascadeMode.Stop;
+
             RuleFor(b => b.Name).NotEmpty();
-            RuleFor(b => b.BookingTime).Matches("[0-9]{1,2}:[0-9][0-9]");
+            RuleFor(b => b.BookingTime)
+                .Matches("[0-9]{1,2}:[0-5][0-9]");
+
+            Transform(x => x.BookingTime, bt => TimeSpan.TryParse(bt, out var result) ? result : TimeSpan.MaxValue)
+                .InclusiveBetween(TimeSpan.FromHours(options.Value.OpenBookingHour), TimeSpan.FromHours(options.Value.ClosedBookingHour));
         }
     }
 }
